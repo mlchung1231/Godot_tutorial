bd7bf4f66575d5b557c302e280f58b5fd5dc083e
mlchung1231 <L35104132@gmail.com>
2023-12-08 04:02:59 +0800

    move_and_shoot

--- player/player.gd
+++ player/player.gd
@@ -40,20 +40,65 @@ func handle_input():
 		shoot()
 		
 
-func _input(event):
-	if event is InputEventScreenTouch or event is InputEventScreenDrag:
-		if $HUD/Joystick/Touch_area.is_pressed():
-			move_vector = calculate_move_vector(event.position)
-			joystick_active = true
-			var joystick_range = event.position.distance_to($HUD/Joystick.position)
-			if joystick_range > 15: 
-				$HUD/Joystick_mark.position = calculate_move_vector(event.position) * 15 + $HUD/Joystick.position
-			else:
-				$HUD/Joystick_mark.position = event.position
+#func _input(event):
+#	if event is InputEventScreenTouch or event is InputEventScreenDrag:
+#		if $HUD/Joystick/Touch_area.is_pressed():
+#			move_vector = calculate_move_vector(event.position)
+#			joystick_active = true
+#			var joystick_range = event.position.distance_to($HUD/Joystick.position)
+#			if joystick_range > 15: 
+#				$HUD/Joystick_mark.position = calculate_move_vector(event.position) * 15 + $HUD/Joystick.position
+#			else:
+#				$HUD/Joystick_mark.position = event.position
+#
+#	if event is InputEventScreenTouch:
+#		if event.pressed == false:
+#			joystick_active = false
 			
+
+var left_hand_index = -1  # 左手触摸事件的索引
+var right_hand_index = -1  # 右手触摸事件的索引
+
+func _input(event):
 	if event is InputEventScreenTouch:
-		if event.pressed == false:
-			joystick_active = false
+		if event.is_pressed():
+			# 检查触摸位置以确定左右手
+			if event.position.x < get_viewport_rect().size.x / 2:
+				# 左手触摸事件
+				left_hand_index = event.index
+				handle_left_hand_touch(event.position)
+			else:
+				# 右手触摸事件
+				right_hand_index = event.index
+				# 右手逻辑（点击）...
+				if $HUD/Attack_button.get_global_rect().has_point(event.position):
+					$HUD/Attack_button.modulate = Color.ORANGE
+					is_shooting = true
+
+		if event.is_pressed() == false:
+			if event.index == left_hand_index:
+				left_hand_index = -1
+				joystick_active = false  # 左手触摸事件结束，禁用操控
+				# 左手触摸事件结束
+			elif event.index == right_hand_index:
+				right_hand_index = -1
+				$HUD/Attack_button.modulate = Color.WHITE
+				is_shooting = false
+				# 右手触摸事件结束
+
+	if event is InputEventScreenDrag:
+		if event.index == left_hand_index:
+			handle_left_hand_touch(event.position)
+
+func handle_left_hand_touch(touch_position):
+	if $HUD/Joystick/Touch_area.is_pressed():
+		move_vector = calculate_move_vector(touch_position)
+		joystick_active = true
+		var joystick_range = touch_position.distance_to($HUD/Joystick.position)
+		if joystick_range > 15: 
+			$HUD/Joystick_mark.position = calculate_move_vector(touch_position) * 15 + $HUD/Joystick.position
+		else:
+			$HUD/Joystick_mark.position = touch_position
 
 func calculate_move_vector(event_position):
 	return (event_position - $HUD/Joystick.position).normalized()	
@@ -77,13 +122,13 @@ func _physics_process(delta):
 	move_and_slide()
 	updatAnimation()
 
-func _on_attack_button_button_down():
-	$HUD/Attack_button.modulate = Color.ORANGE
-	is_shooting = true
-
-func _on_attack_button_button_up():
-	$HUD/Attack_button.modulate = Color.WHITE
-	is_shooting = false
+#func _on_attack_button_button_down():
+#	$HUD/Attack_button.modulate = Color.ORANGE
+#	is_shooting = true
+#
+#func _on_attack_button_button_up():
+#	$HUD/Attack_button.modulate = Color.WHITE
+#	is_shooting = false
 
 func _on_auto_attack_body_entered(body):
 	if body.is_in_group("mob"):
